d08fe1bbceddf8dd8f0c7d4ef457a6595a612462
Yane <yanelobo@gmail.com>
2022-12-24 14:54:54 -0300

    test message

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Cricelia',
+        username: 'Cris',
+        email: 'Cris@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Yane',
+        username: 'Enya',
+        email: 'Yane@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Alana',
+        username: 'Nega',
+        email: 'Alana@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Lurdes',
+        username: 'Lulu',
+        email: 'Lurdes@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Hilana',
+        username: 'Keller',
+        email: 'Hilana@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Alfredo',
+        username: 'Fredo',
+        email: 'Alfredo@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Beatriz',
+        username: 'Bia',
+        email: 'Beatriz@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Jairly',
+        username: 'Jaja',
+        email: 'Jairly@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Priscilla',
+        username: 'Pri',
+        email: 'Priscilla@gmail.com'
+    }
+];
